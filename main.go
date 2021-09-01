9070e8b8321e25ae91ae2684c8abd1a19836c746
berikarg <berikarg@gmail.com>
2021-09-01 23:04:12 +0600

    achieved limit manipulation, added access from browser

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"example.com/binancedepth"
 )
@@ -17,12 +19,32 @@ func main() {
 	}
 	log.SetOutput(logFile)
 
+	http.HandleFunc("/", index)
 	http.HandleFunc("/depth", showDepth)
 	http.ListenAndServe(":8080", nil)
 }
 
+func index(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
 func showDepth(w http.ResponseWriter, r *http.Request) {
-	depth, err := binancedepth.GetDepth("BTCUSDT", 5)
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	r.ParseForm()
+	symbol := r.FormValue("symbol")
+	limitStr := r.FormValue("limit")
+	symbol = strings.TrimPrefix(symbol, "symbol=") //for some reason ParseForm() unnecessarily adds keys
+	limitStr = strings.TrimPrefix(limitStr, "limit=")
+
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	depth, err := binancedepth.GetDepth(symbol, limit)
 	if err != nil {
 		log.Println(err)
 		return
@@ -35,6 +57,8 @@ func showDepth(w http.ResponseWriter, r *http.Request) {
 	//Set Content-Type header so that clients will know how to read response
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, X-Request-With")
 	w.WriteHeader(http.StatusOK)
 	//Write json response back to response
 	w.Write(depthJson)
